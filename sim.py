f7236cf2aa6dda64506a7376baa3ff5bba3e02ce
LSY <2tmdduq19@gmail.com>
2022-08-20 15:33:44 +0900

    sim.py added

---
+++ sim.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from tabnanny import verbose
+from turtle import position
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import cvxpy as cp
+import time
+# matplotlib.rcParams['text.usetex'] = True
+
+from mpl_toolkits.mplot3d import axes3d, Axes3D
+from matplotlib.axes import Axes
